3c9411822c90495aed1bddabb644e64cbadd2a58
Armen M <armen.matoosian@gmail.com>
2023-05-30 17:08:05 -0400

    Add existing file

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# print("Test")
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# sample_file = open(r"C:\Users\armen\Desktop\Opening a File using Python project.txt", "r")
+# print(sample_file.read())
+
+# print(open(r"C:\Users\armen\Desktop\Opening a File using Python project.txt", "r").read())
+
+
+
+# os.system("notepad++ Opening a File using Python project.txt")
+
+# subprocess.Popen(["notepad++", "Opening a File using Python project.txt"])
+# subprocess.Popen(["C:\\Program Files\\Notepad++\\notepad++.exe", "Opening a File using Python project.txt"])
+
+os.system("notepad++ C:\\Users\\armen\\Desktop\\Opening a File using Python project.txt")
